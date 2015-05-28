ab64f75890156d0526d6b627fb62d07669991d55
Markus Chanth <markus.chanth@suomi24.fi>
2015-05-28 09:03:47 +0300

    27.5.2015 Progress Game notes Some of the Sprites

---
+++ Elementa/Assets/MapGen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject tile = Instantiate(Resources.Load("Gridparts_0"),transform.position, transform.rotation) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
